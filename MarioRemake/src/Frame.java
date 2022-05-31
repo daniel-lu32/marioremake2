842e309a9fa41f004104d1ba571344866214031a
daniel-lu32 <90793223+daniel-lu32@users.noreply.github.com>
2022-05-30 21:41:50 -0700

    Frame Comment

--- MarioRemake/src/Frame.java
+++ MarioRemake/src/Frame.java
@@ -17,8 +17,7 @@ import java.awt.Font;
 public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
 	
 	/**
-	 * Game header
-	 * 
+	 * THIS IS JUST A TESTER CLASS.
 	 */
 	
 	// Static Variables
