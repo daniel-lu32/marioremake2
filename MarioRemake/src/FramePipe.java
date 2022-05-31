ab876b24cb89a46d1b9925bb3c4c910f316fa430
daniel-lu32 <90793223+daniel-lu32@users.noreply.github.com>
2022-05-30 21:41:39 -0700

    FramePipe Comment

--- MarioRemake/src/FramePipe.java
+++ MarioRemake/src/FramePipe.java
@@ -17,8 +17,7 @@ import java.awt.Font;
 public class FramePipe extends JPanel implements ActionListener, MouseListener, KeyListener {
 	
 	/**
-	 * Game header
-	 * 
+	 * THIS IS JUST A TESTER CLASS.
 	 */
 	
 	// Static Variables
